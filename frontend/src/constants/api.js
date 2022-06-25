ded729aa0dcab12ac3b80a7465ae9fbcbf6e4cb3
Siddeshwar <siddeshwar.work@gmail.com>
2022-06-25 14:40:25 +0530

    fixing the production server URI in react

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://pizza-plaza.herokuapp.com/';
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://pizza-plaza.herokuapp.com';
 
 export const FIREBASE_API_KEY = 'AIzaSyBHC3KTXgiPy3hS7J3zmAU5Ov9SND10TzA';
